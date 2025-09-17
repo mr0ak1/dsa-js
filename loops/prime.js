b3fbe6bba17036f807ff7891794f2ed16cf44930
mr0ak1 <abhishekkrtiwary3395@gmail.com>
2025-09-17 19:17:20 +0530

    prime update

--- loops/prime.js
+++ loops/prime.js
@@ -1,15 +1,35 @@
 n = 71
 
 if (isNaN(n)) {
-  console.log("Please enter higher number")
+  console.log("Enter valid number")
 } else {
+  let prime = true
   if (n > 0) {
-    let prime = true
-    for (let i = 2; i < Math.floor(n / 2); i++) {
-      if (n % i === 0) {
-        prime = false
-      }
-    }
-    console.log(prime)
+    console.log(isPrime(n))
   }
 }
+
+function isPrime(n) {
+  if (n < 1) {
+    return false
+  }
+
+  if (n === 1) {
+    return true
+  }
+  if (n === 2) {
+    return true
+  }
+  if (n % 2 === 0) {
+    return false
+  }
+  for (let i = 3; i <= Math.floor(Math.sqrt(n)); i++) {
+    if (n % i === 0) {
+      return false
+    }
+  }
+
+  return true
+}
+
+isPrime(n)
